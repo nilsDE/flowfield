7829a652bf84e6499d3c2d4c513b8890f0d21319
Nils Rohe <nilsrohe@freenet.de>
2018-12-30 19:29:38 +0100

    flowfield created

--- sketch.js
+++ sketch.js
@@ -3,27 +3,35 @@ let rows;
 let scl = 10;
 let inc = 0.1;
 let fr;
+let zoff = 0;
 
 function setup() {
     createCanvas(200, 200);
-    background(100);
     cols = floor(width/scl);
 		rows = floor(height/scl);
 		fr = createP('');
 }
 
 function draw() {
-    let yoff = 0;
-    for (let y = 0; y < rows; y++) {
-        let xoff = 0;
-        for (let x = 0; x < cols; x++) {
-            let index = (x+y*width)*4;
-            let r = noise(xoff, yoff)*255;
-            xoff += inc; 
-            fill(r);
-            rect(x*scl, y*scl,scl,scl);
-        }
-        yoff += inc;
-		}
+	background(255);
+  let yoff = 0;
+  for (let y = 0; y < rows; y++) {
+      let xoff = 0;
+      for (let x = 0; x < cols; x++) {
+          let index = (x+y*width)*4;
+					let angle = noise(xoff, yoff, zoff)*TWO_PI;
+					let v = p5.Vector.fromAngle(angle);
+          xoff += inc; 
+					stroke(0);
+					push();
+					translate(x*scl, y* scl);
+					rotate(v.heading());
+					line(0, 0, scl, 0);
+					pop();
+      }
+			yoff += inc;
+			
+			zoff += 0.001;
+	}
 fr.html(floor(frameRate()));		
 }
