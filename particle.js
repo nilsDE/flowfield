842f0b4a028096d29cf7fd4d8903fbe152619e81
Nils Rohe <nilsrohe@freenet.de>
2018-12-30 20:12:33 +0100

    added flowing particles

--- particle.js
+++ particle.js
@@ -1,13 +1,25 @@
 
 function Particle() {
     this.pos = createVector(random(width), random(height));
-    this.vel = p5.Vector.random2D();
+    this.vel = createVector(0, 0);
     this.acc = createVector(0, 0);
+    this.maxspeed = 2;
+
+    this.prevPos = this.pos.copy();
 
     this.update = function() {
         this.vel.add(this.acc);
+        this.vel.limit(this.maxspeed);
         this.pos.add(this.vel);
         this.acc.mult(0);
+    };
+
+    this.follow = function(vectors) {
+        let x = floor(this.pos.x /scl);
+        let y = floor(this.pos.y /scl);
+        let index = x + y * cols;
+        let force = vectors[index];
+        this.applyForce(force);
     };
 
     this.applyForce = function(force) {
@@ -15,14 +27,34 @@ function Particle() {
     };
 
     this.show = function() {
-        stroke(0);
-        point(this.pos.x, this.pos.y);
+        stroke(0, 5);
+        strokeWeight(1);
+        line(this.pos.x, this.pos.y, this.prevPos.x, this.prevPos.y);
+//        point(this.pos.x, this.pos.y);
+        this.updatePrev();
     };    
 
+    this.updatePrev = function() {
+        this.prevPos.x = this.pos.x;
+        this.prevPos.y = this.pos.y;
+    }
+
     this.edges = function() {
-        if (this.pos.x > width) this.pos.x = 0;
-        if (this.pos.x < 0) this.pos.x = width;
-        if (this.pos.y > height) this.pos.y = 0;
-        if (this.pos.y < 0) this.pos.y = height;
+        if (this.pos.x > width) {
+            this.pos.x = 0;
+            this.updatePrev();
+        }
+        if (this.pos.x < 0) {
+            this.pos.x = width;
+            this.updatePrev();
+        }
+        if (this.pos.y > height) {
+            this.pos.y = 0;
+            this.updatePrev();
+        }
+        if (this.pos.y < 0) {
+            this.pos.y = height;
+            this.updatePrev();
+        }
     }
 }
--- sketch.js
+++ sketch.js
@@ -5,44 +5,50 @@ let inc = 0.1;
 let fr;
 let zoff = 0;
 let particles = [];
+let flowfield;
 
 function setup() {
-    createCanvas(200, 200);
-    cols = floor(width/scl);
-		rows = floor(height/scl);
-		fr = createP('');
+	background(255);
+  createCanvas(800, 600);
+  cols = floor(width/scl);
+	rows = floor(height/scl);
+	fr = createP('');
+	flowfield = new Array(cols*rows);
 
-		for (let i =0; i < 100; i++) {
-			particles[i] = new Particle();
-		}
+	for (let i =0; i < 1500; i++) {
+		particles[i] = new Particle();
+	}
 }
 
 function draw() {
-	background(255);
   let yoff = 0;
   for (let y = 0; y < rows; y++) {
       let xoff = 0;
       for (let x = 0; x < cols; x++) {
-          let index = (x+y*width)*4;
-					let angle = noise(xoff, yoff, zoff)*TWO_PI;
+          let index = x+y*cols;
+					let angle = noise(xoff, yoff, zoff)*TWO_PI*4;
 					let v = p5.Vector.fromAngle(angle);
+					v.setMag(1);
+					flowfield[index] = v;
           xoff += inc; 
-					stroke(0);
-					push();
-					translate(x*scl, y* scl);
-					rotate(v.heading());
+//					stroke(0, 50);
+//					push();
+//					translate(x*scl, y* scl);
+//					rotate(v.heading());
+//					strokeWeight(1);
 //					line(0, 0, scl, 0);
-					pop();
+//					pop();
       }
 			yoff += inc;
 			
-			zoff += 0.001;
+			zoff += 0.0003;
 	}
 
 	for (let particle of particles) {
+		particle.follow(flowfield);
 		particle.update();
-		particle.show();
 		particle.edges();
+		particle.show();
 	}
 
 fr.html(floor(frameRate()));		
